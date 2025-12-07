b5ff1567b14dcc0687dac522aaa3860c2f297f31
chikkibum <bhaskarpandeycontacts@gmail.com>
2025-12-07 19:13:39 +0530

    Add sunlight background effects and enhance global styles
    - Introduced new CSS variables for different times of day and shadow effects in globals.css.
    - Added sunlight background component to layout for enhanced visual effects.
    - Updated layout structure to include sunlight background and adjusted theme provider settings.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { generateMetadata as getMetadata } from '@/config/Meta';
 import ReactLenis from 'lenis/react';
 import { ViewTransitions } from 'next-view-transitions';
 import './globals.css';
+import SunlightBackground from '@/components/sunlightBg/sunlight-background';
 
 export const metadata = getMetadata('/');
 
@@ -24,16 +25,17 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+            // enableSystem
+            // disableTransitionOnChange
           >
+        <SunlightBackground>
             <ReactLenis root>
               <div className="relative min-h-screen">
                 {/* Left side pattern */}
                 <div className="fixed top-0 left-0 h-full w-0 xs:w-[5%] sm:w-[10%] lg:w-[20%] xl:w-[25%] border-r border-edge  z-0" />
                 
                 {/* Right side pattern */}
-                <div className="fixed top-0 right-0 h-full w-0 xs:w-[5%] sm:w-[10%] lg:w-[20%] xl:w-[25%] border-l border-4 border-edge z-0" />
+                <div className="fixed top-0 right-0 h-full w-0 xs:w-[5%] sm:w-[10%] lg:w-[20%] xl:w-[25%] border-l border-edge  z-0" />
                 
                 {/* Main content wrapper with padding */}
                 <div className="relative z-10 xs:px-[5%] sm:px-[10%] lg:px-[20%] xl:px-[25%]">
@@ -45,8 +47,9 @@ export default function RootLayout({
                   <ChatBubble />
                 </div>
               </div>
-              <UmamiAnalytics />
+              <UmamiAnalytics />  
             </ReactLenis>
+            </SunlightBackground>
           </ThemeProvider>
         </body>
       </html>
